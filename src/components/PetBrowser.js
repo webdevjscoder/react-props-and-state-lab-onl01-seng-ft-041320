88015aa6dee6bac357c267099cb2b7922eba5154
Juan Cobian <cobianjuan19@gmail.com>
2020-08-20 18:35:44 +0000

    Automatically backed up by Learn

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,14 +4,17 @@ import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
-    const renderPets = this.props.pets.map((pet, index) =>
-      <Pet pet={pet} key={index} isAdopted={this.props.adoptedPets.includes(pet.id)} onAdoptPet={this.props.onAdoptPet} />
-    )
+  	const renderPets = this.props.pets.map((p,i) =>
+  		<Pet pet={p} key={i}
+  		isAdopted={this.props.adoptedPets.includes(p.id)}
+  		onAdoptPet={this.props.onAdoptPet}
+  		/>
+  	)
     return (
       <div className="ui cards">
         {renderPets}
       </div>
-    )
+    );
   }
 }
 
